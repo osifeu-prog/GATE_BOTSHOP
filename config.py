3a5f8697f63ffb9a216a2012cb929f7eb11fcc42
Osif ungar <osif.e.u@gmail.com>
2025-11-25 01:46:46 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # הגדרות סביבה
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 WEBHOOK_URL = os.environ.get('WEBHOOK_URL', 'https://web-production-b425.up.railway.app') + '/webhook'
-MAIN_GROUP_LINK = os.environ.get('MAIN_GROUP_LINK') 
+MAIN_GROUP_LINK = os.environ.get('MAIN_GROUP_LINK', 'https://t.me/your_main_group')
 ADMIN_GROUP_ID = os.environ.get('ADMIN_GROUP_ID', '-1002147033592')
 PAYMENT_CONFIRMATION_GROUP = os.environ.get('PAYMENT_CONFIRMATION_GROUP', '-1002147033592')
 MAIN_COMMUNITY_GROUP = os.environ.get('MAIN_COMMUNITY_GROUP', '-1002147033592')
