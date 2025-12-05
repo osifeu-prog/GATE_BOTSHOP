6611ce239021974a12bfc70bfd3478001d1e317a
Osif ungar <osif.e.u@gmail.com>
2025-12-05 17:48:49 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 import logging
-from pydantic import BaseSettings, Field
 from typing import List, Optional
+
+from pydantic_settings import BaseSettings
+from pydantic import Field
 
 
 class Settings(BaseSettings):
