aae9499018af290357256cf079f0ef241638382d
Osif ungar <osif.e.u@gmail.com>
2025-12-05 21:54:30 +0200

    Update bot_app.py

--- app/telegram_bot/bot_app.py
+++ app/telegram_bot/bot_app.py
@@ -3,6 +3,7 @@ from telegram.ext import Application
 from ..config import settings
 from .handlers.start import register_start_handlers
 from .handlers.settings import register_settings_handlers
+from .handlers.wallet import register_wallet_handlers
 
 
 def build_telegram_app() -> Application:
@@ -13,7 +14,11 @@ def build_telegram_app() -> Application:
         .build()
     )
 
+    # פקודות התחלה והגדרות
     register_start_handlers(app)
     register_settings_handlers(app)
+
+    # ארנק
+    register_wallet_handlers(app)
 
     return app
