20788e6f30e7fc1496abd6b5605fb8f89ef3f462
Osif ungar <osif.e.u@gmail.com>
2026-03-07 16:39:55 +0200

    Update users.py

--- app/models/users.py
+++ app/models/users.py
@@ -8,7 +8,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    telegram_id = Column(Integer, unique=True, nullable=False)
+    from sqlalchemy import BigInteger  telegram_id = Column(BigInteger, unique=True, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     # Identity / Trust
